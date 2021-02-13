5dc344f3db455a070d164b629a57c52ec5d940ce
Christian Knermann <63782465+cknermann@users.noreply.github.com>
2021-02-13 16:38:00 +0100

    Updated script wine-on-chromeos.sh to launch WINE configuration.

--- wine-on-chromeos.sh
+++ wine-on-chromeos.sh
@@ -1,8 +1,9 @@
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
+rm -f winehq.key
 printf "deb https://dl.winehq.org/wine-builds/debian/ buster main\ndeb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./\n" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys DFA175A75104960E
 sudo apt update
 sudo apt install --yes --install-recommends winehq-stable
-rm -f winehq.key
+winecfg
